75c107a4bce61b2390d1c605dec8e716b8db8b45
Tevin Campbell <tevincampbell73@yahoo.com>
2024-07-24 20:15:35 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+const App = () => {
+  return(
+    <>
+      <div>
+        <header>
+          <h2>logo</h2>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App;
